770ac4e449ef703d132daf376cbfe49aa105bb8a
adam-saland <adam.saland@openfin.co>
2020-09-28 08:15:56 -0400

    exit process on fin disconnect

--- build-utils/openfinPlugin.js
+++ build-utils/openfinPlugin.js
@@ -1,15 +1,27 @@
-const { launch } = require('openfin-adapter');
+const { launch, connect } = require('openfin-adapter');
 
 let firstLaunch = true;
+const launchWatch = async (url) => {
+    const port = await launch({ manifestUrl: url });
+
+    //We will use the port to connect from Node to determine when OpenFin exists.
+    const fin = await connect({
+        uuid: 'server-connection', //Supply an addressable Id for the connection
+        address: `ws://localhost:${port}`, //Connect to the given port.
+        nonPersistent: true //We want OpenFin to exit as our application exists.
+    });
+
+    fin.once('disconnected', process.exit)
+}
+
 module.exports = class OpenFinPlugin {
     constructor({ manifestUrl }) {
         this.manifestUrl = manifestUrl;
     }
-    apply(compiler) {
+        apply(compiler) {
         compiler.hooks.done.tap('OpenFinPlugin', (compilation, callback) => {
             if (firstLaunch) {
-                console.log('launching app at: ' + this.manifestUrl)
-                launch({ manifestUrl: this.manifestUrl });
+                launchWatch(this.manifestUrl).then(console.log).catch(console.error)
                 firstLaunch = false;
             }
         })
